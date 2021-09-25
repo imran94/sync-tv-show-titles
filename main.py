b7f3f1cd6b19d8d57aebce6cba7e617d08b64346
Imran <iraja094@gmail.com>
2021-09-26 00:17:27 +0500

    Refactoring

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def rename_files(directory, episodes):
     incomplete_pattern = re.compile("S[0-9]+\s")
     pattern = re.compile("S[0-9]+E[0-9]+]")
     for entry in os.listdir(directory):
+        newname = entry
         if os.path.isdir(os.path.join(directory, entry)):
             print(entry + " is directory")
             rename_files(os.path.join(directory, entry), episodes)
@@ -19,36 +20,34 @@ def rename_files(directory, episodes):
             continue
 
         if re.search(incomplete_pattern, entry):
-            print(entry, end='')
-            entry = re.sub(incomplete_pattern, '', entry).strip()
-            print(" renamed to " + entry)
+            newname = re.sub(incomplete_pattern, '', entry).strip()
 
-        name = re.sub(".(mp4|avi|mpg|mkv)", "", entry)
+        name = re.sub(".(mp4|avi|mpg|mkv)", "", newname)
         for label in episodes:
             if name.lower() in episodes[label].lower():
-                print(label + " - " + entry)
-                episodes.pop(label)
+                newname = label + " - " + newname
+                print(entry + " renamed to " + newname)
+                os.rename(os.path.join(directory, entry),
+                          os.path.join(directory, newname))
                 break
 
-url = "https://thetvdb.com/series/%name%/allseasons/official"
-series_name = sys.argv[1]
-url = url.replace("%name%", series_name)
+if __name__ == "__main__":
+    url = "https://thetvdb.com/series/%name%/allseasons/official"
+    series_name = sys.argv[1]
+    url = url.replace("%name%", series_name)
 
-directory = os.getcwd()
-if len(sys.argv) > 2:
-    directory = sys.argv[2]
+    directory = os.getcwd()
+    if len(sys.argv) > 2:
+        directory = sys.argv[2]
 
-page = requests.get(url)
-soup = BeautifulSoup(page.content, "lxml")
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "lxml")
 
-episodes = {}
-for title in soup.find_all("h4", class_="list-group-item-heading"):
-    title = str(title)
-    title = re.sub('<.*?>', '', title)
-    content = re.findall("[0-9A-Za-z][0-9A-Za-z ,.!?';:]+", title)
-    episodes[content[0]] = content[1]
+    episodes = {}
+    for title in soup.find_all("h4", class_="list-group-item-heading"):
+        title = str(title)
+        title = re.sub('<.*?>', '', title)
+        content = re.findall("[0-9A-Za-z][0-9A-Za-z ,.!?';:]+", title)
+        episodes[content[0]] = content[1]
 
-print("Episode length before: " + str(len(episodes)))
-rename_files(directory, episodes)
-print("Episode length before: " + str(len(episodes)))
-
+    rename_files(directory, episodes)

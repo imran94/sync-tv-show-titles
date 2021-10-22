1dc6d6d8311681ea08705826448a5ad935456919
imran94 <iraja094@gmail.com>
2021-10-22 22:22:19 +0500

    Some extra filtering of name

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ def rename_files(directory, episodes):
     for entry in os.listdir(directory):
         newname = entry
         if os.path.isdir(os.path.join(directory, entry)):
-            print(entry + " is directory")
             rename_files(os.path.join(directory, entry), episodes)
             continue
 
@@ -22,17 +21,25 @@ def rename_files(directory, episodes):
         if re.search(incomplete_pattern, entry):
             newname = re.sub(incomplete_pattern, '', entry).strip()
 
-        name = re.sub(".(mp4|avi|mpg|mkv)", "", newname)
         for label in episodes:
-            if name.lower() in episodes[label].lower():
-                newname = label + " - " + newname
+            if filter_name(newname) in filter_name(episodes[label]):
+                newname = label + " - " + episodes[label]
                 print(entry + " renamed to " + newname)
                 os.rename(os.path.join(directory, entry),
                           os.path.join(directory, newname))
                 break
 
+
+def filter_name(name):
+    name = re.sub(".(mp4|avi|mpg|mkv)", "", name)
+    name = re.sub("[\,\'\.\[\]\!\?\:\-]*", "", name)
+    name = re.sub("([\s]{2,})*", " ", name)
+    name = name.strip().lower()
+    return name
+
+
 if __name__ == "__main__":
-    url = "https://thetvdb.com/series/%name%/allseasons/official"
+    url = "https://thetvdb.com/series/%name%/allseasons/dvd"
     series_name = sys.argv[1]
     url = url.replace("%name%", series_name)
 
@@ -41,6 +48,10 @@ if __name__ == "__main__":
         directory = sys.argv[2]
 
     page = requests.get(url)
+    if page.status_code != 200:
+        url = url.replace("/dvd", "/official")
+        page = requests.get(url)
+
     soup = BeautifulSoup(page.content, "lxml")
 
     episodes = {}
